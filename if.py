7af10d6004423a4238dde7a00109d02e7565cc04
StrictUser <ermolenko198528@gmail.com>
2018-07-30 00:39:22 +0300

    Task #3
    Completed task #3 from Homework #7

--- if.py
+++ if.py
@@ -1,16 +1,49 @@
-try:
-    old = int(input('Ваш вік: '))
-    print('Рекомендовано:', end=' ')
+## First task
+
+#     old = int(input('Ваш вік: '))
+#     print('Рекомендовано:', end=' ')
     
-    if 3 <= old < 6:
-        print('"Важко, але я зможу"')   
-    elif 6 <= old < 12:
-        print('"Хочу одружитись на Мерлін Монро"')
-    elif 12 <= old < 16:
-        print('"Я люблю Леонардо Ді Капріо"')
-    elif 16 <= old:
-        print('"я люблю CURSOR "')
+#     if 3 <= old < 6:
+#         print('"Важко, але я зможу"')   
+#     elif 6 <= old < 12:
+#         print('"Хочу одружитись на Мерлін Монро"')
+#     elif 12 <= old < 16:
+#         print('"Я люблю Леонардо Ді Капріо"')
+#     elif 16 <= old:
+#         print('"я люблю CURSOR "')
+#     else:
+#         print('"Слишком мал для этого!"')
+
+## Second task
+
+# try:
+#     old = int(input('Ваш вік: '))
+#     print('Рекомендовано:', end=' ')
+    
+#     if 3 <= old < 6:
+#         print('"Важко, але я зможу"')   
+#     elif 6 <= old < 12:
+#         print('"Хочу одружитись на Мерлін Монро"')
+#     elif 12 <= old < 16:
+#         print('"Я люблю Леонардо Ді Капріо"')
+#     elif 16 <= old:
+#         print('"я люблю CURSOR "')
+#     else:
+#         print('"Слишком мал для этого!"')
+# except ValueError:
+#     print("Вы ввели не целое число!")
+
+## Third task
+
+try:
+    num = int(input('Enter some number: '))
+    print('You entered:', end=' ')
+    
+    if num > 0:
+        print("1")   
+    elif num < 0:
+        print("-1")
     else:
-        print('"Слишком мал для этого!"')
+        print(0)
 except ValueError:
-    print("Вы ввели не целое число!")
+    print("You entered not integer!")
